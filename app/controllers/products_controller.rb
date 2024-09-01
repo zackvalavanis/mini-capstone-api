36d647e453295764d2d7eb1fc55ac39c93ce305a
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-01 16:21:32 -0500

    Restful Crud

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,14 @@
-class StoreItemsController < ApplicationController
-  def product_things
+class ProductsController < ApplicationController
+  def index
     @products = Product.all
     render template: "products/index"
   end
-
-  def one_product
+  def show 
     @product = Product.find_by(id: params["id"])
+    render template: "products/show"
+  end
+  def create
+    @product = Product.new
     render template: "products/show"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,28 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get "/items" => "store_items#product_things" 
-  get "/one_item/:id" => "store_items#one_product" 
+  get "/products" => "products#index"
+  get "/products/:id" => "products#show"
+  post "/products" => "products#create"
+  delete "/products/:id" => 
 
   # Defines the root path route ("/")
   # root "posts#index"
 end
+
+
+# #HTTP Verb	Path	Controller#Action	Used for
+
+# GET	/photos	photos#index	display a list of all photos
+
+# GET	/photos/new	photos#new	return an HTML form for creating a new photo
+
+# POST	/photos	photos#create	create a new photo
+
+# GET	/photos/:id	photos#show	display a specific photo
+
+# GET	/photos/:id/edit	photos#edit	return an HTML form for editing a photo
+
+# PATCH/PUT	/photos/:id	photos#update	update a specific photo
+
+# DELETE	/photos/:id	photos#destroy	delete a specific photo
