3d97ad5a1b718971f5a51465f5baaff9d0482cb6
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-04 19:20:20 -0500

    methods inside of a class

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    if price <= 10
+      p true
+    else p false
+    end
+  end
+
+  def tax 
+    tax = price * 0.09
+  end
+  def total 
+    price + tax 
+  end 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_01_181955) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_04_015614) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.float "price"
     t.string "image_url"
     t.string "description"
     t.datetime "created_at", null: false
