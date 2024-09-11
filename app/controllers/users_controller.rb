8f3c9268388535c956e5fb3771d1bc352b68864d
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-11 18:32:40 -0500

    updated authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
-    def create
-      user = User.new( 
+  def create
+    user = User.new( 
       name: params[:name],
       email: params[:email],
       password: params[:password]
       )
-      if user.save 
-        render json: { message: 'User has been successfully created!'}, status: :created
-      else 
-        render json: { errors: user.errors.full_message}, status: :bad_request
-      end 
-    end
+    if user.save 
+      render json: { message: 'User has been successfully created!'}, status: :created
+    else 
+      render json: { errors: user.errors.full_message}, status: :bad_request
+    end 
+  end
 end
