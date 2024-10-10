6a0bd45b969150bb0b9fbd984b2af60dbb3879cc
Zack <168221270+Zval321@users.noreply.github.com>
2024-10-09 19:48:35 -0500

    backend running correctly

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,6 +1,6 @@
 class CartedProductsController < ApplicationController
   def index
-    @carted_products = CartedProduct.where(status:['carted', 'purchased'])
+    @carted_products = CartedProduct.where(user_id: current_user.id, status: 'carted')
     render :index
   end   
 
@@ -23,7 +23,7 @@ class CartedProductsController < ApplicationController
 
   def create
     @carted_product = CartedProduct.new(
-      user_id: params[:user_id], 
+      user_id: current_user.id, 
       product_id: params[:product_id], 
       quantity: params[:quantity], 
       status: params[:status]
