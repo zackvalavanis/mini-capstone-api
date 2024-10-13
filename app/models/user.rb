7ad79356a2c3b6680f13badc37d29f51c43b8916
Zack <168221270+Zval321@users.noreply.github.com>
2024-10-13 10:49:29 -0500

    added validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true, length: { maximum: 20 } 
+  validates :password, presence: true, length: { minimum: 10}
   
 
   has_many :orders
